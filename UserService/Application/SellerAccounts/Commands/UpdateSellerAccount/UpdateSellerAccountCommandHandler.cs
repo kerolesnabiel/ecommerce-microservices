fe36cc441de51cfa94be58b5c0298d4691a20dd6
kerolesnabill <keronabilzaki@gmail.com>
2025-03-11 17:40:18 +0200

    feat(UserService): Implement UpdateSellerAccount functionality with command, handler, validator, repository, and controller

---
+++ UserService/Application/SellerAccounts/Commands/UpdateSellerAccount/UpdateSellerAccountCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using UserService.Application.Users;
+using UserService.Domain.Exceptions;
+using UserService.Domain.Interfaces;
+
+namespace UserService.Application.SellerAccounts.Commands.UpdateSellerAccount;
+
+public class UpdateSellerAccountCommandHandler(
+        ILogger<UpdateSellerAccountCommandHandler> logger,
+        ISellerAccountRepository sellerAccountRepository,
+        IUserRepository userRepository,
+        IUserContext userContext,
+        IMapper mapper
+    ) : IRequestHandler<UpdateSellerAccountCommand>
+{
+
+    public async Task Handle(UpdateSellerAccountCommand request, CancellationToken cancellationToken)
+    {
+        var currentUser = userContext.GetCurrentUser();
+        logger.LogInformation("Updating seller account for user: {UserId}", currentUser.Id);
+
+        var sellerAccount = await sellerAccountRepository.GetByUserIdAsync(currentUser.Id)
+            ?? throw new SellerNotFoundException(currentUser.Id.ToString());
+
+        mapper.Map(request, sellerAccount);
+
+        await sellerAccountRepository.UpdateAsync(sellerAccount);
+    }
+}
---
+++ UserService/Domain/Exceptions/SellerNotFoundException.cs
@@ -0,0 +1,5 @@
+﻿namespace UserService.Domain.Exceptions;
+
+public class SellerNotFoundException(string userId) : Exception($"Seller account not found for the user with id: {userId}")
+{
+}
