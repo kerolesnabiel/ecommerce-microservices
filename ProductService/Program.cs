66df138dcee6d3a4c08e5c699466e629966955ae
kerolesnabill <keronabilzaki@gmail.com>
2025-03-24 14:47:23 +0200

    feat(ProductService): Implement JWT authentication with public key retrieval

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,5 +1,6 @@
 using ProductService.Behaviors;
 using ProductService.Middlewares;
+using ProductService.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly;
@@ -19,7 +20,13 @@ builder.Services.AddMarten(cfg => cfg.Connection
 builder.Services.AddCarter();
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
 
+builder.Services.AddAuthorization();
+builder.Services.AddAuthenticationService(builder.Configuration);
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
 app.MapCarter();
