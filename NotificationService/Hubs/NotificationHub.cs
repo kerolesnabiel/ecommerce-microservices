68daddfd49a86522efddf641f4249e8dade0bce0
kerolesnabiel <keronabilzaki@gmail.com>
2025-04-17 13:17:01 +0200

    feat(NotificationService): Add SignalR hub for notifications and update Program.cs for configuration

---
+++ NotificationService/Hubs/NotificationHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace NotificationService.Hubs;
+
+[Authorize]
+public class NotificationHub : Hub
+{
+    private readonly Dictionary<Guid, string> usersConnections = [];
+
+    public async override Task OnConnectedAsync()
+    {
+        bool valid = Guid.TryParse(Context.UserIdentifier, out Guid userId);
+        if (valid)
+            usersConnections[userId] = Context.ConnectionId;
+
+        await base.OnConnectedAsync();
+    }
+
+    public async override Task OnDisconnectedAsync(Exception? exception)
+    {
+
+        bool valid = Guid.TryParse(Context.UserIdentifier, out Guid userId);
+        if (valid)
+            usersConnections.Remove(userId);
+
+        await base.OnDisconnectedAsync(exception);
+    }
+}
--- NotificationService/Program.cs
+++ NotificationService/Program.cs
@@ -1,14 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using NotificationService.Data;
 using NotificationService.Data.Seeders;
+using NotificationService.Hubs;
+using BuildingBlocks.Extensions.ServiceCollection;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
 builder.Services.AddDbContext<NotificationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
+    options.UseNpgsql(config.GetConnectionString("Database")));
+
 builder.Services.AddScoped<Seeder>();
+builder.Services.AddSignalR();
+builder.Services.AddAuthenticationService(config);
 
 var app = builder.Build();
+
 var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<Seeder>();
 await seeder.Seed();
+
+app.MapHub<NotificationHub>("/api/notification/hub");
+
 app.Run();
